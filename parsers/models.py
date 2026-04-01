c58de4be8121527cea5af20e3116753e0aea7646
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-01 20:50:17 +0300

    Rename `ParsedLetters` model to `Letters` and add `is_sent` field

--- parsers/models.py
+++ parsers/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class ParsedLetters(models.Model):
+class Letters(models.Model):
     external_id = models.CharField(max_length=255, unique=True)
     user_id = models.CharField(max_length=255)
     email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
+    is_sent = models.BooleanField(default=False)
