1e51feeec91a6958773ce59df1717f7051052703
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-01 20:25:02 +0300

    Add `ParsedLetters` model to `parsers` app

--- parsers/models.py
+++ parsers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ParsedLetters(models.Model):
+    external_id = models.CharField(max_length=255, unique=True)
+    user_id = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
